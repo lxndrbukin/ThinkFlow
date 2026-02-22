24791e6dfcad4e24feff1c4460dedf479b181f2d
Alexander Bukin <lxndrbukin@proton.me>
2026-02-22 12:54:49 +0000

    Connect Redux Toolkit for state management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
 import App from "./components/App";
 
 const rootDiv = document.querySelector("#root");
 
 if (rootDiv) {
-  createRoot(rootDiv).render(<App />);
+  createRoot(rootDiv).render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
 }
