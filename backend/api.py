78ee38585a8dd0011ffb519dee60303ae905dc1c
Alexander Bukin <lxndrbukin@proton.me>
2026-02-27 14:32:09 +0000

    Update CORS

--- backend/api.py
+++ backend/api.py
@@ -3,11 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers.chat import chats_router
 from routers.notes import notes_router
 
-app = FastAPI(title="AI Assistant")
+app = FastAPI(title="ThinkFlow")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://thinkflow-eosin.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
