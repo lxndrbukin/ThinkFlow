370fa235d01bc6206704fe93754c3a365bd1062c
Alexander Bukin <lxndrbukin@proton.me>
2026-03-03 21:42:35 +0000

    Temporary removal of model selection

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,9 +1,8 @@
 import { type JSX } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import {
   type AppDispatch,
-  type RootState,
   streamMessage,
   createChat,
   addMessage,
@@ -13,14 +12,13 @@ import MessageInput from "../ChatWindow/MessageInput";
 
 export default function Home(): JSX.Element {
   const dispatch = useDispatch<AppDispatch>();
-  const {model} = useSelector((state: RootState) => state.chats.currentChat)
   const navigate = useNavigate();
 
   const handleSend = async (message: string) => {
     const chat = await dispatch(createChat()).unwrap();
     dispatch(clearMessages());
     dispatch(addMessage({ role: "user", content: message }));
-    dispatch(streamMessage({ chatId: chat.id, message, model }));
+    dispatch(streamMessage({ chatId: chat.id, message }));
     navigate(`/chat/${chat.id}`);
   };
 
