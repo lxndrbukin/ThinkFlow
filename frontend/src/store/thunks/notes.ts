8c8929d64f34f6a3c8755ef591173fffb954bd3b
Alexander Bukin <lxndrbukin@proton.me>
2026-03-31 07:15:44 +0100

    Complete notes thunks

--- frontend/src/store/thunks/notes.ts
+++ frontend/src/store/thunks/notes.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import type { NoteCreate, NoteEdit } from "./types";
 import { API_URL } from "../../api";
 
 export const getNotes = createAsyncThunk("notes/getNotes", async () => {
@@ -15,4 +16,26 @@ export const getNote = createAsyncThunk(
   },
 );
 
-export const createNot = createAsyncThunk("notes/createNote", async () => {});
+export const createNote = createAsyncThunk(
+  "notes/createNote",
+  async (data: NoteCreate) => {
+    const response = await axios.post(`${API_URL}/notes`, data);
+    return response.data;
+  },
+);
+
+export const editNote = createAsyncThunk(
+  "notes/editNote",
+  async (data: NoteEdit) => {
+    const response = await axios.put(`${API_URL}/notes/${data.id}`, data);
+    return response.data;
+  },
+);
+
+export const deleteNote = createAsyncThunk(
+  "notes/deleteNote",
+  async (noteId: number) => {
+    await axios.delete(`${API_URL}/notes/${noteId}`);
+    return noteId;
+  },
+);
--- frontend/src/store/thunks/types/index.ts
+++ frontend/src/store/thunks/types/index.ts
@@ -6,8 +6,25 @@ export const Priority = {
 
 export type Priority = (typeof Priority)[keyof typeof Priority];
 
+export const Status = {
+  PENDING: "pending",
+  IN_PROGRESS: "in progress",
+  COMPLETED: "completed",
+} as const;
+
+export type Status = (typeof Status)[keyof typeof Status];
+
 export type NoteCreate = {
-  title: number;
+  title: string;
   desc: string | undefined;
   priority: Priority;
+  status: Status;
+};
+
+export type NoteEdit = {
+  id: number;
+  title: string | undefined;
+  desc: string | undefined;
+  priority: Priority | undefined;
+  status: Status | undefined;
 };
